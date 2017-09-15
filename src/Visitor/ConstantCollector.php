61e589d7f9fe75b8612bf0ffb03859b44b2fb006
Alain Schlesser <alain.schlesser@gmail.com>
2017-09-15 17:40:31 +0200

    Clean up ConstantCollector visitor.

--- src/Visitor/ConstantCollector.php
+++ src/Visitor/ConstantCollector.php
@@ -87,33 +87,13 @@ final class ConstantCollector extends NodeVisitorAbstract
     public function enterNode(Node $node)
     {
         if ($node instanceof File) {
-            $this->file = $node->name;
+            $this->file = $node;
             return null;
         }
 
-        if ($node instanceof Expr\FuncCall && isset($node->name->parts[0]) && $node->name->parts[0] === 'define') {
-            $name = $node->args[0]->value->value ?? null;
-
-            if (null === $name) {
-                return null;
-            }
-
-            $value = $node->args[1]->value ?? null;
-
-            $constant = array_key_exists($name, $this->constants)
-                ? $this->constants[$name]
-                : new Entry\DefineConstant($name);
-
-            $constant = $constant->withLocation(
-                new Location\Declaration(
-                    $this->file,
-                    (int)$node->getAttribute('startLine', '-1'),
-                    (int)$node->getAttribute('endLine', '-1'),
-                    $value
-                )
-            )->withComment($node->getDocComment());
-
-            $this->constants[$name] = $constant;
+        if ($node instanceof Expr\FuncCall
+            && $this->isNamed($node, 'define')) {
+            $this->addDefineConstantEntry($node);
         }
 
         return null;
@@ -129,5 +109,54 @@ final class ConstantCollector extends NodeVisitorAbstract
     public function getConstants(): array
     {
         return $this->constants;
+    }
+
+    /**
+     * Check whether the node has a specific name.
+     *
+     * @since 0.1.0
+     *
+     * @param Node   $node Node to check.
+     * @param string $name Name that needs to be matched.
+     *
+     * @return bool Whether the node has the specified name.
+     */
+    private function isNamed(Node $node, string $name): bool
+    {
+        return isset($node->name->parts)
+            && (string)$node->name === $name;
+    }
+
+    /**
+     * Add DefineConstantEntry.
+     *
+     * @since 0.1.0
+     *
+     * @param Node $node Node to parse.
+     */
+    private function addDefineConstantEntry(Node $node)
+    {
+        $name = $node->args[0]->value->value ?? null;
+
+        if (null === $name) {
+            return;
+        }
+
+        $value = $node->args[1]->value ?? null;
+
+        $constant = array_key_exists($name, $this->constants)
+            ? $this->constants[$name]
+            : new Entry\DefineConstant($name);
+
+        $constant = $constant->withLocation(
+            new Location\Declaration(
+                (string)$this->file->name,
+                (int)$node->getAttribute('startLine', '-1'),
+                (int)$node->getAttribute('endLine', '-1'),
+                $value
+            )
+        )->withComment($node->getDocComment());
+
+        $this->constants[$name] = $constant;
     }
 }
